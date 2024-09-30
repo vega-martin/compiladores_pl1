8470e1f7398ee69fa9c0cc95356ae16e979cd1f9
vega-martin <vega.martin@edu.uah.es>
2024-09-30 20:01:26 +0200

    avanzar estados de salto

--- pl1/src/main/java/src/Automata.java
+++ pl1/src/main/java/src/Automata.java
@@ -13,6 +13,7 @@ public class Automata {
     private List<Integer> estados;
     private int estadoIncicial;
     private List<Integer> estadosFinales;
+    private List<List<Integer>> estadoSalto;
     private HashMap<Integer, HashMap<Character, Integer>> matriz;
     
     public void Automata(){
@@ -40,13 +41,28 @@ public class Automata {
         this.estadosFinales = estadosFinales;
     }
     
+    public void setEstadoSalto(List<List<Integer>> estadoSalto) {
+        this.estadoSalto = estadoSalto;
+    }
+    
     private void inicializarMatriz() {
         for(Integer estado:estados) {
             matriz.put(estado, new HashMap<Character, Integer>());
         }
     }
     
-    private void cargarMatriz(){}
+    private void cargarMatriz(){
+        //parametro lista de listas con los estados de salto
+        int i = 0;
+        int j = 0;
+        for(Integer estado:estados) {
+            for(Character letra:alfabeto) {
+                matriz.get(estado).put(letra, estadoSalto.get(i).get(j));
+                j++;
+            }
+            i++;
+        }
+    }
     
     
 }
