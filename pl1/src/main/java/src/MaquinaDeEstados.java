792fba011c01784a02eac3f100a85f3ed4f26855
vega-martin <vega.martin@edu.uah.es>
2024-10-01 22:12:21 +0200

    empezar maquinaDeEstados

--- pl1/src/main/java/src/MaquinaDeEstados.java
+++ pl1/src/main/java/src/MaquinaDeEstados.java
@@ -1,9 +1,53 @@
 package src;
 
+import java.lang.Exception;
+
 /**
  *
  * @author Vega
  */
 public class MaquinaDeEstados {
+    
+    private Integer estadoActual;
+    private Automata afd;
+    
+    public void inicializar() {
+        estadoActual = afd.getEstadoInicial();
+    }
+    
+    public void aceptaCaracter(Character letra) {
+        Integer estadoTmp = afd.getSiguienteEstado(estadoActual, letra);
+        if(estadoTmp == null) {
+            //throw Exception;
+        }
+    }
+    
+    public boolean isFinal() {
+        return afd.esFinal(estadoActual);
+    }
+    
+    public boolean compruebaCadena(String cadena) {
+        for(int i = 0; i < cadena.length(); i++) {
+            try {
+                this.aceptaCaracter(cadena.charAt(i));
+                if((this.isFinal()) && ((i-cadena.length()) <= 1)) {
+                    return true;
+                }
+            } catch(Exception e) {
+            
+            }
+        }
+        return false;
+    }
+    
+    public String[] generaCadena(int cantidad, int longitud){
+        String cadenasGeneradas[] = new String[cantidad];
+        
+        for(int i = 0; i <= cantidad; i++){
+            // Codigo para generar cadenas
+        }
+        
+        return cadenasGeneradas;
+    }
     
 }
